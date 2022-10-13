54ae349fc5c22aa29480f5c1a30be207e9715a21
Stanislav <stas@0686@mail.ru>
2022-10-13 04:03:39 +0300

    add Example 006_Random

---
+++ Example006_Random/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.Write("первое случайное число ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("второе случайное число ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("сумма этих чисел ");
+Console.WriteLine(result);
